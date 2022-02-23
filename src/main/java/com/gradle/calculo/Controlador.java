b99055e56281f96b22be2fc03559ce14cefd9b05
Diego-Valladares <diego14vb@gmail.com>
2022-02-22 18:55:02 -0600

    Se agrego el main y run a controlador

--- src/main/java/com/gradle/calculo/Controlador.java
+++ src/main/java/com/gradle/calculo/Controlador.java
@@ -1,6 +1,24 @@
 package com.gradle.calculo;
+import java.util.Scanner;
 
 public class Controlador {
-
   
+  public static void main (final String[] args) {
+
+    Controlador controlador = new Controlador();
+
+    controlador.run();
+
+  }
+
+  public void run () {
+
+    Scanner input = new Scanner(System.in);
+
+    input.close();
+
+  }
+
+
+
 }
