e529dd3c70c618d59130cdd1a879adf85791a390
Diego-Valladares <diego14vb@gmail.com>
2022-02-26 15:30:02 -0600

    Updated
    Se agrego un ciclo para quitar comas
    
    Se agregaron variables necesarias
    
    Se agergaron la funcion de suma y multiplicacion de celda a celda

--- src/main/java/com/gradle/calculo/Controlador.java
+++ src/main/java/com/gradle/calculo/Controlador.java
@@ -30,6 +30,12 @@ public class Controlador {
       }
     }
 
+    for (int i = 0; i < rows; i++) {
+      for (int k = 0; k < cols; k++) {
+        datos[i][k] = datos[i][k].replace(',',' ');
+      }
+    }
+
     char parentesis = '(';
 
     this.input.useDelimiter(String.valueOf(parentesis));
@@ -45,6 +51,11 @@ public class Controlador {
       String num2 = " ";
       String den1 = " ";
       String den2 = " ";
+      int row1 = 0;
+      int row2 = 0;
+      int col1 = 0;
+      int col2 = 0;
+
 
       if (comando.equals(">=CEL")) { // Para conseguir la celda que se va a reemplazar
         primerLetra = posiciones.charAt(1);
@@ -64,7 +75,33 @@ public class Controlador {
 
         }
         else {
-        
+          primerLetra = posiciones.charAt(1);
+          row1 = Character.getNumericValue(primerLetra);
+          primerLetra = posiciones.charAt(0);
+          col1 = primerLetra;
+          col1 = col1 - 65;
+
+          primerLetra = posiciones.charAt(3);
+          row2 = Character.getNumericValue(primerLetra);
+          primerLetra = posiciones.charAt(4);
+          col2 = primerLetra;
+          col2 = col1 - 65;
+
+          num2 = "0";
+          den2 = "0";
+          Fraccion f2 = new Fraccion(num2,den2);
+
+          for (int i = row1; i <= row2; i++){
+            for (int k = col1; k <= col2; k++){
+              num1 = datos[row1][col1].split("/")[0];
+              den1 = datos[row1][col1].split("/")[1];
+              Fraccion f1 = new Fraccion(num1, den1);
+              f2 = f2.sumar(f1);
+            }
+          }
+
+          datos[celdaRow][celdaCol] = f2.toString();
+
         }
       }
 
@@ -77,7 +114,32 @@ public class Controlador {
 
         }
         else {
+          primerLetra = posiciones.charAt(1);
+          row1 = Character.getNumericValue(primerLetra);
+          primerLetra = posiciones.charAt(0);
+          col1 = primerLetra;
+          col1 = col1 - 65;
 
+          primerLetra = posiciones.charAt(3);
+          row2 = Character.getNumericValue(primerLetra);
+          primerLetra = posiciones.charAt(4);
+          col2 = primerLetra;
+          col2 = col1 - 65;
+
+          num2 = "1";
+          den2 = "1";
+          Fraccion f2 = new Fraccion(num2,den2);
+
+          for (int i = row1; i <= row2; i++){
+            for (int k = col1; k <= col2; k++){
+              num1 = datos[row1][col1].split("/")[0];
+              den1 = datos[row1][col1].split("/")[1];
+              Fraccion f1 = new Fraccion(num1, den1);
+              f2 = f2.multiplicar(f1);
+            }
+          }
+
+          datos[celdaRow][celdaCol] = f2.toString();
         }
       }
 
