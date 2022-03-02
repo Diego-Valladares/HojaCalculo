3fd6beb96851ee1c7c0615ecfbbc8166789febf9
SebasJC <sebasjica@gmail.com>
2022-03-01 20:46:57 -0600

    Se añadieron comentarios Javadoc y de funcionamiento al método "multConjuntos" de la clase ListaDeFracciones.

--- src/main/java/com/gradle/calculo/ListaDeFracciones.java
+++ src/main/java/com/gradle/calculo/ListaDeFracciones.java
@@ -27,8 +27,9 @@ public class ListaDeFracciones {
 
     //For que cambia el valor de las letras de la celda por el número
     //correspondiente, toma los valores de numeradores y denominadores de la
-    //matriz "datos" y elimina el carácter "/"; si "i" es igual a 1, f2 es igual a
-    //f1, si no, se suman f1 y f2 usando el método "sumar" de la clase Fraccion.
+    //matriz "datos" y elimina el carácter "/"; si "i" es igual a 1, f2 es igual
+    //a f1, si no, se suman f1 y f2 usando el método "sumar" de la clase
+    //Fraccion.
     for (int i = 1; i < arregloConjuntos.length; i++){
       char Letra = arregloConjuntos[i].charAt(1);
       int row = Character.getNumericValue(Letra);
@@ -52,14 +53,30 @@ public class ListaDeFracciones {
     return f2.toString();
   }
 
-
+  /**
+   * Método que se encarga de las multiplicaciones de conjuntos de fracciones.
+   *
+   * @param arregloConjuntos Recibe un arreglo de tipo {@code String[]} con el
+   *                         conjunto de celdas que se van a trabajar.
+   * @param datos Recibe una matriz de tipo {@code String[][]} que tiene los
+   *              datos que fueron ingresados a la hoja de cálculo.
+   * @return Retorna el resultado de la suma.
+   */
   public String multConjunto (String[] arregloConjuntos, String[][] datos) {
 
-
+    //Se establecen numerador y denominador por defecto.
     String num = "1";
     String den = "1";
+
+    //Se crea una nueva instancia de la clase.
     Fraccion f2 = new Fraccion(num, den);
 
+
+    //For que cambia el valor de las letras de la celda por el número
+    //correspondiente, toma los valores de numeradores y denominadores de la
+    //matriz "datos" y elimina el carácter "/"; si "i" es igual a 1, f2 es igual
+    //a f1, si no, se multiplican f1 y f2 usando el método "multiplicar" de la
+    //clase Fraccion.
     for (int i = 1; i < arregloConjuntos.length; i++){
       char Letra = arregloConjuntos[i].charAt(1);
       int row = Character.getNumericValue(Letra);
