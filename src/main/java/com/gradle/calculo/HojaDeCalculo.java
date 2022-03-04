a426e9dd69bcfaa07b40312370e38555cc5140a7
SebasJC <sebasjica@gmail.com>
2022-03-03 20:44:13 -0600

    Adición de documentación javadoc y comentarios internos al método "Imprimir" de la clase HojaDeCalculo.

--- src/main/java/com/gradle/calculo/HojaDeCalculo.java
+++ src/main/java/com/gradle/calculo/HojaDeCalculo.java
@@ -37,10 +37,24 @@ public class HojaDeCalculo
 
   }
 
+  /**
+   * Método que imprime la tabla completa con los cambios realizados.
+   *
+   * @param rows Recibe una variable de tipo {@code int} que indica la cantidad
+   *             de filas que tiene la tabla.
+   * @param cols Recibe una variable de tipo {@code int} que indica la cantidad
+   *             de columnas que tiene la tabla.
+   * @param datos Recibe una matriz de tipo {@code String[][]} que contiene los
+   *              elementos de la matriz ingresada al programa.
+   */
   public static void Imprimir(int rows, int cols, String[][] datos){
-
+    //Variables que se usarán para sacar el mayor número de espacios que ocupará
+    //un elemento de la tabla dentro de lo que se está analizando.
     int size;
     int max = 0;
+
+    //Dos ciclos for que exploran la matriz y sacan la mayor cantidad de
+    // caracteres que ocupa el valor más grande que se analiza.
     for (int i = 0; i < rows; i++){
       for (int k = 0; k < cols; k++){
         size = datos[i][k].length();
@@ -53,21 +67,40 @@ public class HojaDeCalculo
       }
     }
 
+    //Char que almacena las letras del abecedario.
     char [] letras = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    //Espacio de separación de números.
     int espacioNum = 3;
+
+    //Variable que reemplazará tendrá "-" debajo de las letras de las columnas
+    //de la tabla.
     String lineaDivisora = new String(new char[max]).replace("\0",
         "-");
 
+    //Imprimir pared de tabla.
     System.out.print("   |");
+
+    //For que imprime el contenido de "letras" ocupando la cantidad "max" de
+    //caracteres para las letras encima de los números.
     for (int i = 0; i < cols; i ++){
       System.out.printf("%"+max+"C ", letras[i]);
     }
+
+    //Se pasa a la siguiente línea donde se imprime "---+" como parte de la
+    //tabla.
     System.out.println();
     System.out.print("---+");
+
+    //For que imprime las líneas divisorias debajo de las letras.
     for (int i = 0; i < cols; i ++){
       System.out.printf("%s ", lineaDivisora);
     }
+
+    //Salto de línea.
     System.out.println();
+
+    //Dos ciclos for que imprimen los datos de la matriz en la tabla.
     for (int i = 0; i < rows; i ++){
       System.out.printf("%"+espacioNum+"d|", i+1);
       for (int k = 0; k < cols; k++){
